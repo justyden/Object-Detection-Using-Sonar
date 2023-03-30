46c73e9ed7c37043255aca65cf0d5f35d77f3195
Tyler Thompson <tylert123@yahoo.com>
2023-03-30 14:18:05 -0400

    Add files via upload

---
+++ CMPSC 497 Lab 7/CMPSC 497 Lab 7.ino
@@ -0,0 +1,201 @@
+// This program moves forward until an object is detected.
+// Once the object is detected it is determined where it is
+// relative to the robot and moves away from it.
+
+  #include <Servo.h>
+/*Declare L298N Dual H-Bridge Motor Controller directly since there is not a library to load.*/
+//Define L298N Dual H-Bridge Motor Controller Pins
+#define speedPinR 3   // RIGHT PWM pin connect MODEL-X ENA
+#define RightDirectPin1  12    //  Right Motor direction pin 1 to MODEL-X IN1 
+#define RightDirectPin2  11    // Right Motor direction pin 2 to MODEL-X IN2
+#define speedPinL 6        //  Left PWM pin connect MODEL-X ENB
+#define LeftDirectPin1  7    // Left Motor direction pin 1 to MODEL-X IN3
+#define LeftDirectPin2  8   ///Left Motor direction pin 1 to MODEL-X IN4
+#define LPT 2 // scan loop coumter
+
+#define SERVO_PIN     9  //servo connect to D9
+
+#define Echo_PIN    2 // Ultrasonic Echo pin connect to D11
+#define Trig_PIN    10  // Ultrasonic Trig pin connect to D12
+
+#define BUZZ_PIN     13
+#define FAST_SPEED  250     //both sides of the motor speed
+#define SPEED  120     //both sides of the motor speed
+#define TURN_SPEED  200     //both sides of the motor speed
+#define BACK_SPEED1  255     //back speed
+#define BACK_SPEED2  90     //back speed
+
+int leftscanval, centerscanval, rightscanval, ldiagonalscanval, rdiagonalscanval;
+const int distancelimit = 30; //distance limit for obstacles in front           
+const int sidedistancelimit = 30; //minimum distance in cm to obstacles at both sides (the car will allow a shorter distance sideways)
+int distance;
+int numcycles = 0;
+const int turntime = 250; //Time the robot spends turning (miliseconds)
+const int backtime = 300; //Time the robot spends turning (miliseconds)
+
+int thereis;
+Servo head;
+/*motor control*/
+void go_Advance(void)  //Forward
+{
+  digitalWrite(RightDirectPin1, HIGH);
+  digitalWrite(RightDirectPin2,LOW);
+  digitalWrite(LeftDirectPin1,HIGH);
+  digitalWrite(LeftDirectPin2,LOW);
+  set_MotorSpeed(160, 160);
+}
+void go_Left()  //Turn left
+{
+  digitalWrite(RightDirectPin1, HIGH);
+  digitalWrite(RightDirectPin2,LOW);
+  digitalWrite(LeftDirectPin1,LOW);
+  digitalWrite(LeftDirectPin2,HIGH);
+  set_MotorSpeed(160, 160);
+}
+void go_Right()  //Turn right
+{
+  digitalWrite(RightDirectPin1, LOW);
+  digitalWrite(RightDirectPin2,HIGH);
+  digitalWrite(LeftDirectPin1,HIGH);
+  digitalWrite(LeftDirectPin2,LOW);
+  set_MotorSpeed(160, 160);
+}
+void go_Back()  //Reverse
+{
+  digitalWrite(RightDirectPin1, LOW);
+  digitalWrite(RightDirectPin2,HIGH);
+  digitalWrite(LeftDirectPin1,LOW);
+  digitalWrite(LeftDirectPin2,HIGH);
+  set_MotorSpeed(160, 160);
+}
+void stop_Stop()    //Stop
+{
+  digitalWrite(RightDirectPin1, LOW);
+  digitalWrite(RightDirectPin2,LOW);
+  digitalWrite(LeftDirectPin1,LOW);
+  digitalWrite(LeftDirectPin2,LOW);
+  set_MotorSpeed(0,0);
+}
+
+/*set motor speed */
+void set_MotorSpeed(int speed_L,int speed_R)
+{
+  analogWrite(speedPinL,speed_L); 
+  analogWrite(speedPinR,speed_R);   
+}
+
+void buzz_ON()   //open buzzer
+{
+  
+  for(int i=0;i<100;i++)
+  {
+   digitalWrite(BUZZ_PIN,LOW);
+   delay(2);//wait for 1ms
+   digitalWrite(BUZZ_PIN,HIGH);
+   delay(2);//wait for 1ms
+  }
+}
+void buzz_OFF()  //close buzzer
+{
+  digitalWrite(BUZZ_PIN, HIGH);
+  
+}
+void alarm(){
+   buzz_ON();
+ 
+   buzz_OFF();
+}
+
+/*detection of ultrasonic distance*/
+int watch(){
+  long echo_distance;
+  digitalWrite(Trig_PIN,LOW);
+  delayMicroseconds(5);                                                                              
+  digitalWrite(Trig_PIN,HIGH);
+  delayMicroseconds(15);
+  digitalWrite(Trig_PIN,LOW);
+  echo_distance=pulseIn(Echo_PIN,HIGH);
+  echo_distance=echo_distance*0.01657; //how far away is the object in cm
+  //Serial.println((int)echo_distance);
+  return round(echo_distance);
+}
+
+
+// Moves the robot forward until something is detected.
+void checkForObjects() { 
+  head.write(90); // Sets sonar position to straight.
+  delay(200);
+
+  while (watch() > 20) { // Goes forward if nothing is in front of the robot.
+    go_Advance();
+  }
+  
+  stop_Stop(); // Something was detected in front.
+  delay(1000);
+  alarm(); // The alarm signals that it detected an object.
+
+  int obstacle_On_Right = 0;
+  int obstacle_On_Left = 0;
+
+  head.write(130); // Sonar to left side.
+  delay(200);
+  if (watch() < 25) { // If something is in range set a value.
+    obstacle_On_Left = 1;
+  }
+
+  head.write(50); // Sonar to right side.
+  delay(200);
+  if (watch() < 25) { // If something is in range set a value.
+    obstacle_On_Right = 1;
+  }
+
+  if (obstacle_On_Right == 1 && obstacle_On_Left == 0) { // Checks to see if an object was only on right.
+    go_Left(); // Moves robot left.
+    delay(300);
+  }
+
+  else if (obstacle_On_Right == 0 && obstacle_On_Left == 1) { // Checks to see if an object was only on left.
+    go_Right(); // Moves robot right.
+    delay(300);
+  }
+
+  else { // Object was detected on both sides.
+    go_Back(); // Moves the robot back.
+    delay(1000);
+    go_Right(); // Turns the robot to the right to get out of way of object.
+    delay(250);
+  }
+}
+
+void setup() {
+  /*setup L298N pin mode*/
+  pinMode(RightDirectPin1, OUTPUT); 
+  pinMode(RightDirectPin2, OUTPUT); 
+  pinMode(speedPinL, OUTPUT);  
+  pinMode(LeftDirectPin1, OUTPUT);
+  pinMode(LeftDirectPin2, OUTPUT); 
+  pinMode(speedPinR, OUTPUT); 
+  stop_Stop();//stop move
+  /*init HC-SR04*/
+  pinMode(Trig_PIN, OUTPUT); 
+  pinMode(Echo_PIN,INPUT); 
+  /*init buzzer*/
+  pinMode(BUZZ_PIN, OUTPUT);
+  digitalWrite(BUZZ_PIN, HIGH);  
+  buzz_OFF(); 
+
+  digitalWrite(Trig_PIN,LOW);
+  /*init servo*/
+  head.attach(SERVO_PIN); 
+  head.write(90);
+   delay(2000);
+  
+  Serial.begin(9600);
+ 
+ 
+}
+
+void loop() {
+  checkForObjects();
+  delay(300);
+}
